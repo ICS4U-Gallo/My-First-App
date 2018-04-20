544ae8ce2300aaa0b2ace0e698555f6ef3dda136
robuntu <robuntu@robuntu.ca>
2018-04-20 09:41:16 -0400

    video 15

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,62 @@
+package controllers;
+
+import models.Book;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import views.html.books.*;
+
+import javax.inject.Inject;
+import java.util.Set;
+
+public class BooksController extends Controller{
+
+    @Inject
+    FormFactory formFactory;
+
+    // for all books
+
+    public Result index(){
+        Set<Book> books = Book.allBooks();
+        return ok(index.render(books));
+    }
+
+    // to create book
+
+    public Result create(){
+        Form<Book> bookForm = formFactory.form(Book.class);
+        return ok(create.render(bookForm));
+    }
+
+    // to save book
+
+    public Result save(){
+        Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
+        Book book = bookForm.get();
+        Book.add(book);
+        return redirect(routes.BooksController.index());
+    }
+
+    public Result edit(Integer id){
+        return TODO;
+    }
+
+    public Result update(){
+        return TODO;
+    }
+
+    public Result destroy(Integer id){
+        return TODO;
+    }
+
+    // for book details
+
+    public Result show(Integer id){
+        return TODO;
+    }
+
+
+
+}
